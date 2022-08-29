2425f648d57397ddd921fc2103fb4c0b77cf8a22
izzy417 <israeldail2@gmail.com>
2022-08-29 23:42:28 +0000

    jwt

--- src/api/routes.py
+++ src/api/routes.py
@@ -4,15 +4,35 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
 
 api = Blueprint('api', __name__)
 
 
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
+@api.route("/login", methods=["GET", "POST"])
+def create_token():
+    data = request.get_json()
 
-    response_body = {
-        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
-    }
+    if "email" not in data:
+        raise APIException('Email Not Found in request', status_code=400)
+    if "password" not in data:
+        raise APIException('Password Not Found in request', status_code=400)
 
-    return jsonify(response_body), 200
+    user = User.query.filter_by(email=data["email"]).first()
+
+    if user is None:
+        raise APIException(
+            'Profile does not exist, please check credentials', status_code=404)
+    if data["password"] != user.password:
+        raise APIException('Password is incorrect', status_code=401)
+
+    access_token = create_access_token(identity=data["email"])
+    print("just say something")
+    print(access_token)
+    # response = make_response(
+    #     jsonify({'message': 'login successfully', 'access_token': access_token}), 200,)
+    # response.headers["Content-Type"] = "application/json"
+
+    return jsonify(access_token=access_token), 200
