6a642558683d2671033bcd793fcd297ae8c05422
izzy417 <israeldail2@gmail.com>
2022-08-30 00:09:33 +0000

    removed jwt

--- src/app.py
+++ src/app.py
@@ -11,7 +11,8 @@ from api.models import db
 from api.routes import api
 from api.admin import setup_admin
 from api.commands import setup_commands
-from flask_jwt_extended import JWTManager
+
+
 
 #from models import Person
 
@@ -21,8 +22,7 @@ static_file_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
-app.config["JWT_SECRET_KEY"] = os.environ.get('JWT_SECRET', 'sample key') # Change this!
-jwt = JWTManager(app)
+
 
 # database condiguration
 db_url = os.getenv("DATABASE_URL")
